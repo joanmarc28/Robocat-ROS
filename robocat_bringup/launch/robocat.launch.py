0ed46e211479eae5e7a22ab67bbcdf09e15d2e5c
Joan Marc Samó Rojas <22conan12@gmail.com>
2026-01-19 20:33:31 +0100

    Mes canvis

--- robocat_bringup/launch/robocat.launch.py
+++ robocat_bringup/launch/robocat.launch.py
@@ -7,6 +7,9 @@ def generate_launch_description():
     pairing_params = PathJoinSubstitution(
         [FindPackageShare("robocat_bringup"), "config", "pairing.yaml"]
     )
+    telemetry_params = PathJoinSubstitution(
+        [FindPackageShare("robocat_bringup"), "config", "telemetry.yaml"]
+    )
     return LaunchDescription([
         Node(
             package="robocat_pairing",
@@ -16,5 +19,11 @@ def generate_launch_description():
         ),
         Node(package="robocat_hw", executable="oled_message_node", output="screen"),
         Node(package="robocat_hw", executable="telemetry_node", output="screen"),
+        Node(
+            package="robocat_hw",
+            executable="web_telemetry_node",
+            output="screen",
+            parameters=[telemetry_params],
+        ),
         Node(package="robocat_control", executable="cmd_node", output="screen"),
     ])
