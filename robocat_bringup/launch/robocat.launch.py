deff4b239e27124286c43927eaa4cdbcd850ca97
Joan Marc Samó Rojas <22conan12@gmail.com>
2026-01-19 13:18:32 +0100

    Update robocat.launch.py

--- robocat_bringup/launch/robocat.launch.py
+++ robocat_bringup/launch/robocat.launch.py
@@ -1,9 +1,19 @@
 from launch import LaunchDescription
+from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
+    pairing_params = PathJoinSubstitution(
+        [FindPackageShare("robocat_bringup"), "config", "pairing.yaml"]
+    )
     return LaunchDescription([
-        Node(package="robocat_pairing", executable="pairing_node", output="screen"),
+        Node(
+            package="robocat_pairing",
+            executable="pairing_node",
+            output="screen",
+            parameters=[pairing_params],
+        ),
         Node(package="robocat_hw", executable="oled_message_node", output="screen"),
         Node(package="robocat_hw", executable="telemetry_node", output="screen"),
         Node(package="robocat_control", executable="cmd_node", output="screen"),
